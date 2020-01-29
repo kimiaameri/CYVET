aeabd26320b794a259ef58e46abba1ade923a336
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2020-01-29 05:20:07 -0600

    Update scrap_web.py

--- scrap_web.py
+++ scrap_web.py
@@ -16,22 +16,40 @@ for i in range(14):
     for row1 in table_items:
       vendor = row1.h3.text
       print(vendor)
-      table_field=table_main.findAll('span', attrs={'class':'views-field views-field-title'})
+      need = {}
+      for li_tag in soup.find_all('ul'):
+        for span_tag in li_tag.find_all('li', {'class':'views-field views-field-title'}):
+           field = span_tag.find('span', {'class':'field-content'}).text
+           print(field)
+           link = span_tag.find('span', {'class':'field-content'}).a
+           print(link)
+           csv_writer.writerow([vendor, product, link])
+
+print(need)
+      table_field=table_items.findAll('span', attrs={'class':'views-field views-field-title'})
       for row2 in table_field:
+        if vendor in 
         product=row2.text
         print(product)
         link = row2.a['href']
         print(link)
-        csv_writer.writerow(['vendor', 'product', 'link']
+        csv_writer.writerow([vendor, product, link])
 csv_file.close()
 
 for i in range(14):
     url = 'https://www.us-cert.gov/ics/advisories-by-vendor?page={}'.format(i)
     r = requests.get(url) 
-    soup = BeautifulSoup(r.content, 'html.parser') 
+    soup = BeautifulSoup(r.content, 'lxml') 
     table_main = soup.find('div', attrs = {'class':'view-content'}) 
     table_items =table_main.findAll('div', attrs = {'class':'item-list'})
     for row1 in table_items:
       vendor = row1.h3.text
       print(vendor)
-      csv_writer.writerow([vendor, 'product', 'link'])
+      table_field=table_items.findAll('span', attrs={'class':'views-field views-field-title'})
+      for row2 in table_field:
+        if vendor in 
+        product=row2.text
+        print(product)
+        link = row2.a['href']
+        print(link)
+        csv_writer.writerow([vendor, product, link])
